65609b68c3fc03213f6d37f46e159b0e506baedf
Gimyoeun <gimhyoeun80@gmail.com>
2024-01-30 16:09:25 +0900

    feat : use useQuery for fetching

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -1,73 +1,45 @@
-import { useEffect, useState } from 'react';
-
+import { useQuery } from '@tanstack/react-query';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import EventItem from './EventItem.jsx';
+import { fetchEvents } from '../../util/http.js';
 
 export default function NewEventsSection() {
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+    const { data, isPending, isError, error } = useQuery({
+        queryKey: ['events', { max: 3 }],
+        queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
+        staleTime: 5000, // 컴포넌트를 렌더링하고 다시 렌더링을 요청했을 때 5초안에 이 일이 일어나면 요청 하지 않음.// 쓸데없는 요청을 제한
+        //   gcTime: 30000, // 캐시 데이터가 30초 이후에 없어짐.다시 재 요청을 해야함.
+    });
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:3000/events');
+    let content;
 
-      if (!response.ok) {
-        const error = new Error('An error occurred while fetching the events');
-        error.code = response.status;
-        error.info = await response.json();
-        throw error;
-      }
-
-      const { events } = await response.json();
-
-      return events;
+    if (isPending) {
+        content = <LoadingIndicator />;
     }
 
-    fetchEvents()
-      .then((events) => {
-        setData(events);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+    if (isError) {
+        content = <ErrorBlock title="An error occurred" message={error.info?.message || 'Failed to fetch events.'} />;
+    }
 
-  let content;
+    if (data) {
+        content = (
+            <ul className="events-list">
+                {data.map((event) => (
+                    <li key={event.id}>
+                        <EventItem event={event} />
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 
-  if (isLoading) {
-    content = <LoadingIndicator />;
-  }
-
-  if (error) {
-    content = (
-      <ErrorBlock title="An error occurred" message="Failed to fetch events" />
+    return (
+        <section className="content-section" id="new-events-section">
+            <header>
+                <h2>Recently added events</h2>
+            </header>
+            {content}
+        </section>
     );
-  }
-
-  if (data) {
-    content = (
-      <ul className="events-list">
-        {data.map((event) => (
-          <li key={event.id}>
-            <EventItem event={event} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
-  return (
-    <section className="content-section" id="new-events-section">
-      <header>
-        <h2>Recently added events</h2>
-      </header>
-      {content}
-    </section>
-  );
 }
